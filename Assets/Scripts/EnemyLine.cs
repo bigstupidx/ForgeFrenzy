df2913ae354f43c30af2119961d317ff864073e2
Dave Tamayo <dave_tamayo@hotmail.com>
2017-02-21 17:00:09 -0500

    Started converting game over to the new 3D level

---
+++ Assets/Scripts/EnemyLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLine : MonoBehaviour {
+
+	[SerializeField] float startingZ = 33f;
+	[SerializeField] float endZ = -1f;
+	[SerializeField] float moveSpeed = 1;
+
+	Vector3 endPosition;
+
+	void Awake () {
+
+		this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, startingZ);
+		endPosition = new Vector3(this.transform.position.x, this.transform.position.y, endZ);
+	}
+
+	void Update () {
+
+		this.transform.position = Vector3.Lerp(this.transform.position, endPosition, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[DisallowMultipleComponent]
+public class NewPlayer : MonoBehaviour {
+
+	[SerializeField] int inputID;
+	[SerializeField] float moveSpeed = 0.1f;
+
+	Player player;
+	Rigidbody rb;
+
+	void Awake () {
+
+		player = ReInput.players.GetPlayer(inputID);
+		rb = this.GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+
+		ProcessMovement();
+	}
+
+	void ProcessMovement () {
+
+		float horizontalAxis = player.GetAxis("Horizontal");
+		float verticalAxis = player.GetAxis("Vertical");
+
+		if(horizontalAxis != 0 || verticalAxis != 0) {
+
+			float angleFacing = Mathf.Atan2 (player.GetAxis ("Horizontal"), player.GetAxis ("Vertical")) * Mathf.Rad2Deg;
+			this.transform.rotation = Quaternion.Euler (0, angleFacing, 0);
+
+			rb.MovePosition(this.transform.position + moveSpeed * new Vector3(horizontalAxis, 0, verticalAxis) * Time.deltaTime);
+		}
+	}
+}
