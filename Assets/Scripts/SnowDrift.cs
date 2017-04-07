4684dd3e97677a9ee3e073cb8883f2e08714d6d6
Rebecca <rebeccamitchell88@gmail.com>
2017-04-07 18:31:00 -0400

    Added in a snow drift

---
+++ Assets/Scripts/SnowDrift.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowDrift : MonoBehaviour {
+
+    int dir;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * dir * Time.deltaTime);
+    }
+
+    public void SetDir(int newDir) {
+        dir = newDir;
+    }
+}
---
+++ Assets/Scripts/SnowDriftSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowDriftSpawner : MonoBehaviour {
+
+    [SerializeField] GameObject snowDriftPrefab;
+
+    [SerializeField] float leftX;
+    [SerializeField] float rightX;
+    [SerializeField] float maxZ;
+    [SerializeField] float minZ;
+    float randX;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnSnowDrift());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnSnowDrift() {
+
+        GameObject[] drifts = GameObject.FindGameObjectsWithTag("Snow Drift");
+
+        if (drifts.Length < 2)
+        {
+            float randZ = Random.Range(minZ, maxZ);
+            int randSide = Random.Range(0, 2);
+
+            if (randSide == 0) { randX = rightX; }
+            else { randX = leftX; }
+
+            Vector3 newPosition = new Vector3(randX, 0, randZ);
+            GameObject newDrift = Instantiate(snowDriftPrefab, newPosition, Quaternion.identity) as GameObject;
+
+            if (randSide == 0) { newDrift.GetComponent<SnowDrift>().SetDir(-1); }
+            else { newDrift.GetComponent<SnowDrift>().SetDir(1); }
+        }
+
+        float randWait = Random.Range(5, 10);
+        yield return new WaitForSeconds(randWait);
+
+        StartCoroutine(SpawnSnowDrift());
+    }
+}
