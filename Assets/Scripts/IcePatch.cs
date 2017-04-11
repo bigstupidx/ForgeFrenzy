5039f0d3f867624d8db6f3f259ab52530c6cb451
Rebecca <rebeccamitchell88@gmail.com>
2017-04-11 19:54:57 -0400

    Added new level designs in my scene

--- Assets/Scripts/IcePatch.cs
+++ Assets/Scripts/IcePatch.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class IcePatch : MonoBehaviour {
 
+    [SerializeField] float speedBoost = 1.5f;
+    [SerializeField] float playerSpeed = 0.1f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
-
+            other.GetComponent<PlayerController>().SetMoveSpeed(speedBoost);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player")) {
-
+            other.GetComponent<PlayerController>().SetMoveSpeed(playerSpeed);
         }
     }
 }
